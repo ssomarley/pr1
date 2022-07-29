20fa316787d0bb8fd02a364ab8c9383268bd1052
ssomarley <110258687+ssomarley@users.noreply.github.com>
2022-07-29 12:49:00 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a=10;
+        int a=20;
        // aa
         //salam
         
